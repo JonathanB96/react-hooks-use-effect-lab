73f78219ad47c49ae7c491288a51896ffb828fae
jonathan bola <bolajonathan@yahoo.com>
2022-03-29 19:48:16 -0500

    commit tree ed518fe86ccf21bc65e5c3d5a0f6e8da8831bc35

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  setTimeout(()=>{
+
+  },1000)
+  useEffect(()=>{
+    
+      if(timeRemaining === 0){
+        setTimeRemaining(10)
+        onAnswered(false)          
+       
+       } 
+    const timer = setInterval(()=>{
+        setTimeRemaining(timeRemaining-1)
+
+        },1000)
+
+
+     
+      return function cleanUp(){
+        clearInterval(timer) 
+        clearTimeout(timer)
+      }
+
+       
+     
+  }, [timeRemaining, onAnswered])
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
